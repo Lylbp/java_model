87492d5c0c417e3d0d59f4be452459dcb9c84bf0
alex <weiwenbin@yayuanzi.com>
2020-05-19 08:56:21 +0800

    feat

--- src/main/java/com/dar/road/entity/TbMenu.java
+++ src/main/java/com/dar/road/entity/TbMenu.java
@@ -1,8 +1,11 @@
 package com.dar.road.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Table(name = "tb_menu")
+@Data
 public class TbMenu {
     @Column(name = "menu_id")
     @Id
@@ -19,52 +22,8 @@ public class TbMenu {
     private String pid;
 
     /**
-     * @return menu_id
+     * 是否有效
      */
-    public String getMenuId() {
-        return menuId;
-    }
-
-    /**
-     * @param menuId
-     */
-    public void setMenuId(String menuId) {
-        this.menuId = menuId == null ? null : menuId.trim();
-    }
-
-    /**
-     * 获取前端url
-     *
-     * @return url - 前端url
-     */
-    public String getUrl() {
-        return url;
-    }
-
-    /**
-     * 设置前端url
-     *
-     * @param url 前端url
-     */
-    public void setUrl(String url) {
-        this.url = url == null ? null : url.trim();
-    }
-
-    /**
-     * 获取上级ID
-     *
-     * @return pid - 上级ID
-     */
-    public String getPid() {
-        return pid;
-    }
-
-    /**
-     * 设置上级ID
-     *
-     * @param pid 上级ID
-     */
-    public void setPid(String pid) {
-        this.pid = pid == null ? null : pid.trim();
-    }
+    @Column(name = "is_valid")
+    private Boolean isValid;
 }
--- src/main/java/com/dar/road/entity/TbMenuPermission.java
+++ src/main/java/com/dar/road/entity/TbMenuPermission.java
@@ -1,8 +1,11 @@
 package com.dar.road.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Table(name = "tb_menu_permission")
+@Data
 public class TbMenuPermission {
     /**
      * 菜单与权限id
@@ -24,56 +27,8 @@ public class TbMenuPermission {
     private String permissionId;
 
     /**
-     * 获取菜单与权限id
-     *
-     * @return menu_permission_id - 菜单与权限id
+     * 是否有效
      */
-    public String getMenuPermissionId() {
-        return menuPermissionId;
-    }
-
-    /**
-     * 设置菜单与权限id
-     *
-     * @param menuPermissionId 菜单与权限id
-     */
-    public void setMenuPermissionId(String menuPermissionId) {
-        this.menuPermissionId = menuPermissionId == null ? null : menuPermissionId.trim();
-    }
-
-    /**
-     * 获取菜单id
-     *
-     * @return menu_id - 菜单id
-     */
-    public String getMenuId() {
-        return menuId;
-    }
-
-    /**
-     * 设置菜单id
-     *
-     * @param menuId 菜单id
-     */
-    public void setMenuId(String menuId) {
-        this.menuId = menuId == null ? null : menuId.trim();
-    }
-
-    /**
-     * 获取权限id
-     *
-     * @return permission_id - 权限id
-     */
-    public String getPermissionId() {
-        return permissionId;
-    }
-
-    /**
-     * 设置权限id
-     *
-     * @param permissionId 权限id
-     */
-    public void setPermissionId(String permissionId) {
-        this.permissionId = permissionId == null ? null : permissionId.trim();
-    }
+    @Column(name = "is_valid")
+    private Boolean isValid;
 }
