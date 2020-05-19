9886174e2617d1a32e6a2c8230717c9f92069df2
alex <weiwenbin@yayuanzi.com>
2020-05-19 08:59:23 +0800

    feat

---
+++ src/main/java/com/dar/road/DTO/MenuEditDTO.java
@@ -0,0 +1,11 @@
+package com.dar.road.DTO;
+
+import lombok.Data;
+
+/**
+ * @Author weiwenbin
+ * @Date 2020/5/19 上午8:58
+ */
+@Data
+public class MenuEditDTO {
+}
---
+++ src/main/java/com/dar/road/VO/MenuVO.java
@@ -0,0 +1,12 @@
+package com.dar.road.VO;
+
+import com.dar.road.entity.TbMenu;
+import lombok.Data;
+
+/**
+ * @Author weiwenbin
+ * @Date 2020/5/19 上午8:57
+ */
+@Data
+public class MenuVO extends TbMenu {
+}
