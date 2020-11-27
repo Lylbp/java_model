9395f6015d5c94db41f8f5b0bf466623927214ee
alex <weiwenbin@yayuanzi.com>
2020-11-27 10:22:07 +0800

    feat

--- src/main/java/com/lylbp/JavaModelApplication.java
+++ src/main/java/com/lylbp/JavaModelApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OaApplication {
+public class JavaModelApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OaApplication.class, args);
+        SpringApplication.run(JavaModelApplication.class, args);
     }
 }
