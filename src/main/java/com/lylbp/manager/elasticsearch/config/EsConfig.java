9089b13af33717649fa980c9ffc3a2bde6275384
alex <alex@192.168.150.129>
2021-04-12 15:45:36 +0800

    feat

--- src/main/java/com/lylbp/manager/elasticsearch/config/EsConfig.java
+++ src/main/java/com/lylbp/manager/elasticsearch/config/EsConfig.java
@@ -1,6 +1,9 @@
 package com.lylbp.manager.elasticsearch.config;
 
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -9,6 +12,9 @@ import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfig
 import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 /**
  * @author weiwenbin
  * @date 2020/11/13 下午2:39
@@ -16,6 +22,9 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.lylbp.manager.elasticsearch.demo.repository")
 public class EsConfig extends AbstractElasticsearchConfiguration {
+    @Resource
+    ElasticsearchRestClientProperties esProperties;
+
     /**
      * elasticsearchTemplate 替换为 elasticsearchRestTemplate
      *
@@ -34,6 +43,10 @@ public class EsConfig extends AbstractElasticsearchConfiguration {
      */
     @Override
     public RestHighLevelClient elasticsearchClient() {
-        return RestClients.create(ClientConfiguration.localhost()).rest();
+        String[] hosts = esProperties.getUris().toArray(new String[0]);
+        ClientConfiguration build = ClientConfiguration.builder()
+                .connectedTo(hosts)
+                .build();
+        return RestClients.create(build).rest();
     }
 }
